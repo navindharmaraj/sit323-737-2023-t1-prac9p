60bd416c8357cca73c9b0ca6efaf7e011749994b
Navin Dharmarajan <navindharmaraj@users.noreply.github.com>
2023-05-31 11:46:58 +1000

    demo push

--- model/webCount_model.js
+++ model/webCount_model.js
@@ -2,13 +2,20 @@ require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient
 const devDbUri = process.env.DEV_DB_URI;
 const prodDbUri = process.env.PROD_DB_URI;
-const uri = process.env.NODE_ENV === 'production' ? prodDbUri : devDbUri;
+
+let uri;
+if (process.env.NODE_ENV === 'production') {
+  uri = process.env.DB_URI || prodDbUri;
+} else {
+  uri = process.env.DB_URI || devDbUri;
+}
 const dbclient = new MongoClient(uri, { useNewUrlParser: true });
+
 
 exports.addCollection = async function (collectionName, dataObj) {
 
     var dbConnection = await dbclient.connect();
-    var db = await dbConnection.db('ageig');
+    var db = await dbConnection.db('contacts');
     return await db.collection(collectionName).insertOne(dataObj)
    
 }
